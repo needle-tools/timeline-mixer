0f16bac915e25e6bc9ccc11d054da758c393b675
Marcel Wiessler <marcel@gaisterhand.de>
2021-01-19 12:50:51 +0100

    can inject one other timeline track but not really mapped correctly or handles empty tracks etc. Connecting another timeline with animation mixer is not enough

--- package/Runtime/needle.TimelineMixer/Experimental/ExperimentalTimelineUtilities.cs
+++ package/Runtime/needle.TimelineMixer/Experimental/ExperimentalTimelineUtilities.cs
@@ -1,4 +1,4 @@
-using System.Linq;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.Playables;
@@ -8,44 +8,49 @@ namespace needle.TimelineMixer.Experimental
 {
 	public static class ExperimentalTimelineUtilities
 	{
-		public static PlayableGraph GetGraph(PlayableDirector director, TimelineAsset asset)
+		public static void AddPlayables(PlayableDirector director, TimelineAsset asset, out List<(TrackAsset asset, Playable playable)> playables)
 		{
-			var myGraph = PlayableGraph.Create("Mix: " + asset.name);
+			playables = new List<(TrackAsset asset, Playable playable)>();
+			var graph = director.playableGraph;// PlayableGraph.Create(asset.name + "-MixInstance");
 			foreach (var o in asset.GetOutputTracks())
 			{
-				asset.GetOutputTrack(0).CreatePlayable(myGraph, director.gameObject);
+				var res = o.CreatePlayable(graph, director.gameObject);
+				// if (res.IsValid()) Debug.Log(res);
+				playables.Add((o, res));
 			}
-			return myGraph;
-		}
-		
-		public static ScriptPlayable<TimelinePlayable> AddTimeline(this AnimationLayerMixerPlayable layerMixer, 
-			TimelineAsset asset, PlayableDirector director, PlayableGraph graph, out int index, bool autoBalance = true)
-		{
-			if (!director)
-			{
-				index = -1;
-				return ScriptPlayable<TimelinePlayable>.Null;
-			}
-			
-			var timelinePlayable = TimelinePlayable.Create(graph, asset.GetOutputTracks(), director.gameObject, autoBalance, true);
-			index = layerMixer.AddInput(timelinePlayable, 0);
-			return timelinePlayable;
 		}
 
-		public static ScriptPlayable<TimelinePlayable> AddTimeline(this AnimationLayerMixerPlayable layerMixer, 
-			TimelineAsset asset, GameObject timelineRoot, out int index, bool autoBalance = true)
+		public static ScriptPlayable<TimelinePlayable> AddTimeline(this AnimationLayerMixerPlayable layerMixer,
+			TimelineAsset asset, PlayableDirector director, out int index, bool autoBalance = true)
 		{
-			if (!timelineRoot || !asset)
+			if (!director || !director.playableGraph.IsValid() || !asset)
 			{
 				index = -1;
 				return ScriptPlayable<TimelinePlayable>.Null;
 			}
-		
-			var g = PlayableGraph.Create("MyGraph");
+
+			// AddPlayables(director, asset, out var playables);
+			//
+			// foreach (var (trackAsset, playable) in playables)
+			// {
+			// 	Debug.Log(trackAsset + " - " + trackAsset.duration + ", PlayableIsValid? " + playable.IsValid());
+			// 	// playable.SetDuration(trackAsset.duration);
+			// }
 			
-			
-			var timelinePlayable = TimelinePlayable.Create(g, asset.GetOutputTracks(), timelineRoot, autoBalance, true);
-			index = layerMixer.AddInput(timelinePlayable, 0);
+			var tracks = asset.GetOutputTracks();
+			var timelinePlayable = TimelinePlayable.Create(director.playableGraph, tracks, director.gameObject, autoBalance, true);
+			var mixer = AnimationMixerPlayable.Create(director.playableGraph, timelinePlayable.GetInputCount());
+			for (var i = 0; i < timelinePlayable.GetInputCount(); i++)
+			{
+				var input = timelinePlayable.GetInput(i);
+				Debug.Log(input.GetPlayableType());
+				// timelinePlayable.DisconnectInput(i);
+				// mixer.AddInput(input, 0, 1);
+			}
+
+			mixer.AddInput(timelinePlayable, 0, 1);
+
+			index = layerMixer.AddInput(mixer, 0);
 			return timelinePlayable;
 		}
 	}
--- package/Runtime/needle.TimelineMixer/Experimental/TimelineMixer.cs
+++ package/Runtime/needle.TimelineMixer/Experimental/TimelineMixer.cs
@@ -12,24 +12,32 @@ namespace needle.TimelineMixer.Experimental
 		public TimelineAsset Asset;
 		public float Weight;
 		private int index;
+		public float Duration = 3;
 
-		private PlayableGraph myGraph;
+		// private PlayableGraph myGraph;
 		private ScriptPlayable<TimelinePlayable> timeline;
 
 		public override Animator Animator => animator;
 		public override void OnConnected(PlayableGraph graph, AnimationLayerMixerPlayable mixer)
 		{
-			myGraph = ExperimentalTimelineUtilities.GetGraph(Director, Asset);
-			timeline  = mixer.AddTimeline(Asset, Director, myGraph, out index);
+			timeline = mixer.AddTimeline(Asset, Director, out index);
+			Debug.Log("Duration: " + timeline.GetDuration());
 		}
 
 		public override void OnUpdate(TimelineGraphModificationManager manager, AnimationLayerMixerPlayable mixer)
 		{
-			myGraph.Evaluate(Time.deltaTime);
-			myGraph.SetTimeUpdateMode(DirectorUpdateMode.Manual);
+			// myGraph.Evaluate(Time.deltaTime);
+			// myGraph.SetTimeUpdateMode(DirectorUpdateMode.Manual);
+			timeline.SetTime(timeline.GetTime()+Time.deltaTime);
+			if (timeline.GetTime() > Duration)
+			{
+				timeline.SetTime(0);
+				Debug.Log(timeline.GetTime()); 
+			}
+			if (index < 0) return;
 			mixer.SetInputWeight(0, 1 - Weight);
 			mixer.SetInputWeight(index, Weight);
-			if (timeline.GetTime() > 1.5) timeline.SetTime(0);
+
 		}
 	}
 }
